be69fd1e87023f2ac4f9172d56d6f6abf05c632f
Hemasree837 <hemasree837@gmail.com>
2025-11-23 14:30:58 +0530

    Add recursive array printing functionality
    Implement recursive method to print array elements

---
+++ aArr_rec.java
@@ -0,0 +1,15 @@
+public class aArr_rec {
+    public static void main(String[] args) {
+        int []arr={10,20,30,40,50};
+        printArray(arr,0);
+
+    }
+    public static void printArray(int[] arr,int index){
+        if(index==arr.length-1)
+        {
+            return;
+        }
+        System.out.println(arr[index]+" ");
+        printArray(arr, index+1);
+    }
+}
