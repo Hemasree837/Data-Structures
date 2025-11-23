2062d1d04b7d78aade12eac85769124905b96338
Hemasree837 <hemasree837@gmail.com>
2025-11-23 11:46:27 +0530

    Implement recursive multiplication method

---
+++ annum_mul.java
@@ -0,0 +1,12 @@
+public class annum_mul {
+    public static void main(String[] args) {
+        System.out.println(add(5));
+    }
+    public static int add(int num)
+    {
+        if(num==0) return 1;
+        else return num*add(num-1);
+    }
+}
+
+
