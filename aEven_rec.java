22dbb47c98b61d90a35933d1e36ecc77544dc0c8
Hemasree837 <hemasree837@gmail.com>
2025-11-23 14:07:09 +0530

    Implement recursive functions to check even/odd

---
+++ aEven_rec.java
@@ -0,0 +1,22 @@
+public class aEven_rec {
+    public static void main(String[] args) {
+        System.out.println(isEven(5));
+        System.out.println(isOdd(5));
+    }
+    public static boolean isEven(int n)
+    {
+        if(n==0)
+        {
+            return true;
+        }
+        return isOdd(n-1);
+    }
+    public static boolean isOdd(int n)
+    {
+        if(n==0)
+        {
+            return false;
+        }
+        return isEven(n-1);
+    }
+}
