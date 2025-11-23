cfa1bd4f4a5c3f4c60ab69ea5b9975df31e46e48
Hemasree837 <hemasree837@gmail.com>
2025-11-23 11:40:52 +0530

    Implement recursive addition in annumbers2.java

---
+++ annumbers2.java
@@ -0,0 +1,10 @@
+public class annumbers2 {
+    public static void main(String[] args) {
+        System.out.println(add(5));
+    }
+    public static int add(int num)
+    {
+        if(num==0) return 0;
+        else return num+add(num-1);
+    }
+}
