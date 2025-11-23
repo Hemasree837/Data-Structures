4d881a667a863781ab58b422afb28b2b118984ff
Hemasree837 <hemasree837@gmail.com>
2025-11-23 11:36:27 +0530

    Add recursive print function in astart10.java

---
+++ astart10.java
@@ -0,0 +1,15 @@
+public class astart10 {
+    public static void main(String[] args) {
+        System.out.println(prints(10));
+    }
+    public static int prints(int num)
+    {
+       if(num==0)
+        {
+            System.out.println("Start");
+             return 0;
+        }
+       System.out.println(num);
+       return prints(num-1);
+    }
+}
